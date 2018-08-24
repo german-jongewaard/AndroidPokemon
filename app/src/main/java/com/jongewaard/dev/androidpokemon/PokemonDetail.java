d1e7685ecfedbe224dfcd4d84b4e3ffa91b22a15
german <germanjongewaard@gmail.com>
2018-08-24 16:02:09 +0200

    Pokemon

--- app/src/main/java/com/jongewaard/dev/androidpokemon/PokemonDetail.java
+++ app/src/main/java/com/jongewaard/dev/androidpokemon/PokemonDetail.java
@@ -3,15 +3,33 @@ package com.jongewaard.dev.androidpokemon;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.jongewaard.dev.androidpokemon.Common.Common;
+import com.jongewaard.dev.androidpokemon.model.Pokemon;
+
+import org.w3c.dom.Text;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PokemonDetail extends Fragment {
+
+
+    ImageView pokemon_img;
+    TextView pokemon_name, pokemon_height, pokemon_weight;
+    RecyclerView recycler_type, recycler_weakness, recycler_next_evolution, recycler_prev_evolution;
+
+
+
 
     static PokemonDetail instance;
 
@@ -25,12 +43,61 @@ public class PokemonDetail extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokemon_detail, container, false);
+        View itemView = inflater.inflate(R.layout.fragment_pokemon_detail, container, false);
+
+        Pokemon pokemon;
+        //Getposition from Argument (el bundle que puse en el Main bundle.putInt("position", position);
+        if(getArguments().get("num") == null)
+            pokemon = Common.commonPokeomList.get(getArguments().getInt("position"));
+        else
+            pokemon = null;
+
+        pokemon_img = (ImageView)itemView.findViewById(R.id.pokemon_image);
+        pokemon_name = (TextView) itemView.findViewById(R.id.name);
+        pokemon_height= (TextView) itemView.findViewById(R.id.height);
+        pokemon_weight = (TextView) itemView.findViewById(R.id.weight);
+
+        recycler_type = (RecyclerView)itemView.findViewById(R.id.recyclerview_type);
+        recycler_type.setHasFixedSize(true);
+        recycler_type.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        recycler_weakness = (RecyclerView)itemView.findViewById(R.id.recyclerview_weakness);
+        recycler_weakness.setHasFixedSize(true);
+        recycler_weakness.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        recycler_next_evolution = (RecyclerView)itemView.findViewById(R.id.recyclerview_next_evolution);
+        recycler_next_evolution.setHasFixedSize(true);
+        recycler_next_evolution.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        recycler_prev_evolution = (RecyclerView)itemView.findViewById(R.id.recyclerview_prev_evolution);
+        recycler_prev_evolution.setHasFixedSize(true);
+        recycler_prev_evolution.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        setDetailPokemon(pokemon);
+
+        return itemView;
     }
 
+    private void setDetailPokemon(Pokemon pokemon) {
+
+        //Load Image
+        Glide.with(getActivity()).load(pokemon.getImg()).into(pokemon_img);
+
+        pokemon_name.setText(pokemon.getName());
+
+        pokemon_weight.setText("Weight" + pokemon.getWeight());
+
+        pokemon_height.setText("Height" + pokemon.getHeight());
+
+
+
+
+
+
+
+    }
 }
