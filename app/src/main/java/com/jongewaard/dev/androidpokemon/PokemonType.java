c6d98cc313cf5444f24370ff288493cc3cff2630
german <germanjongewaard@gmail.com>
2018-09-07 09:19:13 +0200

    PokemonType selections

--- app/src/main/java/com/jongewaard/dev/androidpokemon/PokemonType.java
+++ app/src/main/java/com/jongewaard/dev/androidpokemon/PokemonType.java
@@ -3,9 +3,19 @@ package com.jongewaard.dev.androidpokemon;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.jongewaard.dev.androidpokemon.Adapter.PokemonListAdapter;
+import com.jongewaard.dev.androidpokemon.Retrofit.IPokemonDex;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.disposables.CompositeDisposable;
 
 
 /**
@@ -13,6 +23,19 @@ import android.view.ViewGroup;
  */
 public class PokemonType extends Fragment {
 
+
+     RecyclerView pokemon_list_recyclerview;
+
+    PokemonListAdapter adapter, search_adapter;
+    List<String> last_suggest = new ArrayList<>();
+
+    MaterialSearchBar searchBar;
+
+    static PokemonType instance;
+
+    public static PokemonType getInstance() {
+        return instance;
+    }
 
     public PokemonType() {
         // Required empty public constructor
