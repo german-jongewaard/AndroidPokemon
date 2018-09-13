746cd9ab0d1ded9ecddc49f8bf4413c208b40db0
german <germanjongewaard@gmail.com>
2018-09-13 09:34:47 +0200

    PokemonType selections

--- app/src/main/java/com/jongewaard/dev/androidpokemon/Adapter/PokemonTypeAdapter.java
+++ app/src/main/java/com/jongewaard/dev/androidpokemon/Adapter/PokemonTypeAdapter.java
@@ -1,6 +1,7 @@
 package com.jongewaard.dev.androidpokemon.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.RecyclerView;
@@ -63,6 +64,7 @@ public class PokemonTypeAdapter extends RecyclerView.Adapter<PokemonTypeAdapter.
                 public void onChipClick(View v) {
 
                     LocalBroadcastManager.getInstance(mContext)
+                            .sendBroadcast(new Intent(Common.KEY_POKEMON_TYPE).putExtra("type"));
 
                 }
             });
